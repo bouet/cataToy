23b554cc18a5600a4b67b6ac1026b98906b2cbc3
bouet <corentinbp@gmail.com>
2017-09-26 09:35:47 +0200

    Creation de la classe trancheAge

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-console.log("hello world");
 const Jouet = require('./jouet.js');
+const TrancheAge = require('./trancheAge.js');
+
 console.log("Bienvenue dans le catalogue de jouet");
-var unJouet = new Jouet("Draisienne junior bleue");
+let unJouet = new Jouet("Draisienne junior bleue");
 console.log(unJouet.libelle);
+
+    let tranche25 =  new TrancheAge("tranche25",2,5);
+console.log(tranche25.toString());
