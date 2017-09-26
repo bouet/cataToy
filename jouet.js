92c5551c00a763d83b6635ed2cecba785401194e
bouet <corentinbp@gmail.com>
2017-09-26 09:57:17 +0200

    Affectation d'une categorie à un jouet

--- jouet.js
+++ jouet.js
@@ -1,19 +1,25 @@
 class Jouet {
 
 
-    constructor(unLibelle,uneTancheAge){
+    constructor(unLibelle,uneTancheAge,uneCategorie){
         this._libelle = unLibelle;
         this._trancheAge = uneTancheAge;
+        this._categorie = uneCategorie;
+
+        this._categorie.ajouterJouet(this);
     }
 
     get libelle(){
         return this._libelle;
     }
 
-    get trancheAge(){
+    get TrancheAge(){
         return this._trancheAge.toString();
     }
 
+    get Categorie(){
+        return this._categorie.libelle;
+    }
 
 }
 module.exports = Jouet;
